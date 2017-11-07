b7e62eb28c3f3fce9f653de93a0c40521eb9d93c
Gerold55 <marleybeare56@gmail.com>
2017-11-07 13:36:16 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ minetest.register_node('deco_lamp:lamp_off_' ..material, {
 	drawtype = 'mesh',
 	mesh = 'deco_lamp.obj',
 	tiles = {'deco_'..material..'.png'},
-	groups = {cracky=2, oddly_breakably_by_hand=2, not_in_creative_inventory=1},
+	groups = {cracky=2, oddly_breakably_by_hand=2},
 	paramtype = 'light',
 	paramtype2 = 'facedir',
 	selection_box = {
@@ -27,7 +27,7 @@ minetest.register_node('deco_lamp:lamp_off_' ..material, {
 		fixed = {-.35, -.5, -.35, .35, .25, .35},
 		},
 	on_rightclick = function (pos, node, puncher)
-		node.name = "deco_lamp:deco_lamp" ..material
+		node.name = "deco_lamp:deco_lamp_" ..material
 		minetest.set_node(pos, node)
 	end,
 })
@@ -37,11 +37,11 @@ minetest.register_node('deco_lamp:deco_lamp_' ..material, {
 	drawtype = 'mesh',
 	mesh = 'deco_lamp.obj',
 	tiles = {'deco_'..material..'.png'},
-	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=1},
+	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=1, not_in_creative_inventory=1},
 	light_source =  14,
 	paramtype = 'light',
 	paramtype2 = 'facedir',
-	drop= 'deco_lamp:lamp_off',
+	drop= 'deco_lamp:lamp_off' ..material,
 	selection_box = {
 		type = 'fixed',
 		fixed = {-.35, -.5, -.35, .35, .25, .35}, -- Right, Bottom, Back, Left, Top, Front
@@ -51,7 +51,7 @@ minetest.register_node('deco_lamp:deco_lamp_' ..material, {
 		fixed = {-.35, -.5, -.35, .35, .25, .35}, -- Right, Bottom, Back, Left, Top, Front
 		},
 		on_rightclick = function (pos, node, puncher)
-		node.name = "deco_lamp:_lamp_off" ..material
+		node.name = "deco_lamp:lamp_off_" ..material
 		minetest.set_node(pos, node)
 	end,
 })
