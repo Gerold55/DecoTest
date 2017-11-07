90e0f762de3ac8419d8a62b16d516a91ec7520d7
Gerold55 <marleybeare56@gmail.com>
2017-11-07 17:19:58 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -92,3 +92,32 @@ minetest.register_node('deco:table_' ..material, {
 	}
 })
 end
+
+minetest.register_node("deco:deco_painting_1", {
+	description= 'Painting 1',
+	tiles = {
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"painting_1.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, 0, 0.5, 0.5, 0.0625}, -- NodeBox1
+			{-0.5, -0.5, -0.0625, -0.375, 0.5, 0}, -- NodeBox2
+			{0.375, -0.5, -0.0625, 0.5, 0.5, 0}, -- NodeBox3
+			{-0.375, -0.5, -0.0625, 0.375, -0.375, 0}, -- NodeBox4
+			{-0.375, 0.375, -0.0625, 0.375, 0.5, 0.0625}, -- NodeBox5
+			{-0.375, -0.0625, 0.0625, 0.375, 0, 0.125}, -- NodeBox7
+			{-0.375, -0.125, 0.125, 0.375, -0.0625, 0.1875}, -- NodeBox8
+			{-0.375, -0.25, 0.1875, 0.375, -0.125, 0.25}, -- NodeBox9
+			{-0.375, -0.375, 0.25, 0.375, -0.25, 0.3125}, -- NodeBox10
+			{-0.375, -0.5, 0.3125, 0.375, -0.375, 0.375}, -- NodeBox11
+		}
+	}
+})
